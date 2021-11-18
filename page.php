d9ef3e4c65e8fe63759d7aee8d38f27231b1d446
Jeremy <hercmakesthings@gmail.com>
2021-11-18 15:02:19 -0500

    2nd page.php commit

--- page.php
+++ page.php
@@ -10,7 +10,14 @@
 
     <!--main section-->
     <section class="container-fluid mainarea">
-
+    <?php 
+        if(have_posts()){
+          while(have_posts()){
+            the_post();
+            the_content();
+          }
+        }
+      ?>
     </section><!--mainarea section end-->
 
 <!--theme footer-->
