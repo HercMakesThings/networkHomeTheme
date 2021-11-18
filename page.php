fff7de5dcc3620635dd2fb3116659908df64be5c
Jeremy <hercmakesthings@gmail.com>
2021-11-18 14:59:57 -0500

    1st page.php commit

---
+++ page.php
@@ -0,0 +1,17 @@
+<!--theme header-->
+<?php get_header(); ?>
+
+<!--nonhomepagesection start-->
+<section id="nonhomepage">
+
+    <!--landingPage Top Section-->
+    <?php get_template_part('topsec'); ?>
+    <!--landingPage Top Section END-->
+
+    <!--main section-->
+    <section class="container-fluid mainarea">
+
+    </section><!--mainarea section end-->
+
+<!--theme footer-->
+<?php get_footer(); ?>
