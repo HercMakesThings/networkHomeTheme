4e81d1593682d671b061249d593bd5ef6a2827ee
Jeremy <hercmakesthings@gmail.com>
2021-11-21 18:40:01 -0500

    enabled the Graduates buttons on the home page

--- index.php
+++ index.php
@@ -15,17 +15,17 @@
         </div>
         <div data-aos="fade-left" class="col-sm-6 raspsec d-flex flex-column justify-content-center" style="background-image: url('<?php echo wp_get_attachment_url(get_theme_mod('raspsec_identity_bg')); ?>')">
         <h1>Recording Arts & Show Production</h1>
-        <a href="#">Graduates</a>
+        <a href="https://firstportfol.io/rasp/">Graduates</a>
         </div>
     </div>
     <div class="row">
         <div data-aos="fade-right" class="col-sm-6 dfvpsec d-flex flex-column justify-content-center" style="background-image: url('<?php echo wp_get_attachment_url(get_theme_mod('dfvpsec_identity_bg')); ?>')">
         <h1>Digital Filmmaking & Video Production</h1>
-        <a href="#">Graduates</a>
+        <a href="https://firstportfol.io/dfvp/">Graduates</a>
         </div>
         <div data-aos="fade-left" class="col-sm-6 gdwdsec d-flex flex-column justify-content-center" style="background-image: url('<?php echo wp_get_attachment_url(get_theme_mod('gdwdsec_identity_bg')); ?>')">
         <h1>Graphic Design & Web Development</h1>
-        <a href="#">Graduates</a>
+        <a href="https://firstportfol.io/gdwd/">Graduates</a>
         </div>
     </div>
     </section><!--deptsec container END-->
